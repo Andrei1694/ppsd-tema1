466bf361c01820cee1c28ed07522e3efc611d575
Andrei1694 <andrei4.stanciu@gmail.com>
2020-04-06 08:24:13 +0300

    improved Server

---
+++ ppsd_server/Server.java
@@ -0,0 +1,21 @@
+package ppsd_server;
+import java.io.*;
+import java.net.*;
+import  ppsd_server.ClientHandler;
+
+public class Server {
+    public static void main(String args[]) {
+        int port = 8000;
+        int maxConnections = 3;
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while(maxConnections <= 3){
+                Socket clientSocket = serverSocket.accept();
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                clientHandler.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
