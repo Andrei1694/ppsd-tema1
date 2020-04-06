2ee9224a557129bd2d254ef5a46798e0a80603a2
Andrei1694 <andrei4.stanciu@gmail.com>
2020-04-06 06:09:44 +0300

    added Server

---
+++ Server.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    public static void main(String args[]) {
+        int port = 8000;
+        int maxConnections = 3;
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while(maxConnections <= 3){
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+                writer.println("Hello World !");
+                clientSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- ServerMessage.java
+++
@@ -1,5 +0,0 @@
-class ServerMessage {
-    public static void main(String[] args) {
-        System.out.println("LOsdsOL");
-    }
-}
